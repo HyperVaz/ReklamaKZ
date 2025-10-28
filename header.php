2dada72bc3506c1f18f3173de8fa5b822731f0b8
Timur <149872424+HyperVaz@users.noreply.github.com>
2025-10-28 16:25:30 +0300

    добавил остальные страницы

---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Название компании - Наружная реклама</title>
+    <meta name="description"
+          content="Профессиональное изготовление наружной рекламы: вывески, баннеры, рекламные конструкции">
+    <!-- Подключение шрифтов -->
+    <link rel="stylesheet" href="style.css"/>
+
+</head>
+<body>
+<!-- Фон с анимацией -->
+<div class='ripple-bg' data-colors='["#ffea1d", "#ffd909", "#ffc800", "#ffe538", "#ffb81b"]'></div>
+
+<!-- Основной контент -->
+<div class="main-content">
+    <!-- Шапка сайта -->
+    <header class="header">
+        <div class="container">
+            <div class="header-content">
+                <div class="header-content-logo">
+                    <div class="under-logo">
+                        НАРУЖНАЯ
+                    </div>
+                    <hr class="header-hr">
+                    <img src="images/logo.png" alt="Логотип компании" class="logo">
+                </div>
+
+                <div class="contact-info">
+                    <div class="contact-details">
+                        <div class="contact-details-phone">
+                            <p>ежедневно с 9:00 до 18:00</p>
+                            <p>+7(937) 009-07-05</p>
+                        </div>
+                        <nav class="main-nav">
+                            <button class="nav-toggle">☰</button>
+                            <ul>
+                                <li class="current"><a href="index.html">Главная</a></li>
+                                <li><a href="about.html">О нас</a></li>
+                                <li><a href="services.html">Услуги</a></li>
+                                <li><a href="price.html">Цены</a></li>
+                                <li><a href="portfolio.html">Наши работы</a></li>
+                                <li><a href="contacts.html">Контакты</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <a href="https://t.me/your_telegram" class="telegram-btn" target="_blank" aria-label="Telegram">
+                        <img src="images/Telegram_logo.svg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
